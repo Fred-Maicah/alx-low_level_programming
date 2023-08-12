364489f693e397f0c80742094f651a7e66686755
fred maicah <fredmaicah@gmail.com>
2023-08-11 17:25:57 -0700

    My 5-print_numbers.c commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -  a program that prints all single digit numbers of base
+ * 10 starting from 0
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int k;
+for (k = 0; k < 10; k++)
+	printf("%d", k);
+putchar('\n');
+return (0);
+}
