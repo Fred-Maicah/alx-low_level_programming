76490760bd85208521c9e72e89e189b7c3d28d82
fred maicah <fredmaicah@gmail.com>
2023-08-11 18:09:41 -0700

    My 7-print_tebahpla.c commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - a program that prints the lowercase alphabet in reverse
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char k;
+
+k = 'z';
+while
+	(k >= 'a') {
+		putchar(k);
+		k--;
+	}
+putchar('\n');
+return (0);
+}
