87fafbbeeccaacb125331a3997bacd99480a98c0
Fred-Maicah <fredmaicah@gmail.com>
2023-09-25 13:40:15 -0700

    My 7-get_nodeint.c commit correct

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,21 +1,22 @@
 #include "lists.h"
 
 /**
- * sum_listint - calculates the sum of all the data in a listint_t list
+ * get_nodeint_at_index - returns the node at a certain index in a linked list
  * @head: first node in the linked list
+ * @index: index of the node to return
  *
- * Return: resulting sum
+ * Return: pointer to the node we're looking for, or NULL
  */
-int sum_listint(listint_t *head)
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	int sum = 0;
+	unsigned int i = 0;
 	listint_t *temp = head;
 
-	while (temp)
+	while (temp && i < index)
 	{
-		sum += temp->n;
 		temp = temp->next;
+		i++;
 	}
 
-	return (sum);
+	return (temp ? temp : NULL);
 }
